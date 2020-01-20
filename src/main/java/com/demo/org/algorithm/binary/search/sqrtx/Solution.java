f7e55d662a2c8462eac6ce208c0a126e4e23fd01
jonwinters <jonwinters1992@gmail.com>
2020-01-20 09:05:02 +0800

    add sqrtx

---
+++ src/main/java/com/demo/org/algorithm/binary/search/sqrtx/Solution.java
@@ -0,0 +1,38 @@
+package com.demo.org.algorithm.binary.search.sqrtx;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Solution {
+
+    public int mySqrt(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        // 注意：针对特殊测试用例，例如 2147395599
+        // 要把搜索的范围设置成长整型
+        // 为了照顾到 0 把左边界设置为 0
+        long left = 0;
+        // # 为了照顾到 1 把右边界设置为 x // 2 + 1
+        long right = num / 2 + 1;
+        while (left < right) {
+            // 注意：这里一定取右中位数，如果取左中位数，代码会进入死循环
+            // long mid = left + (right - left + 1) / 2;
+            long mid = (left + right + 1) >>> 1;
+            long square = mid * mid;
+            if (square > num) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        // 因为一定存在，因此无需后处理
+        return (int) left;
+    }
+
+    @Test
+    public void testMySqrt() {
+        Assert.assertTrue(mySqrt(8) == 2);
+        Assert.assertTrue(mySqrt(6) == 2);
+    }
+}
